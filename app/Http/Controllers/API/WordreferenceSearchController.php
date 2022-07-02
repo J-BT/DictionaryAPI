74037a868229110d7d5e17d49f48e9b318e3f8ab
J-BT <jeanbaptiste.theroulde@gmail.com>
2022-07-02 20:05:08 +0200

    creation wordReference Controller

---
+++ app/Http/Controllers/API/WordreferenceSearchController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\WordreferenceHistory;
+use Illuminate\Http\Request;
+
+class WordreferenceSearchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\WordreferenceHistory  $wordreferenceHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WordreferenceHistory $wordreferenceHistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\WordreferenceHistory  $wordreferenceHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WordreferenceHistory $wordreferenceHistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\WordreferenceHistory  $wordreferenceHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WordreferenceHistory $wordreferenceHistory)
+    {
+        //
+    }
+}
