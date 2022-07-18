2929ee154ba42197cb2911e46685cbf8242a8bae
J-BT <jeanbaptiste.theroulde@gmail.com>
2022-07-18 18:23:37 +0200

    Jquery OK ; ToDo : AJAX calls from list.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,33 @@
 import './bootstrap';
+import $ from 'jquery';
+window.$ = window.jQuery = $;
 
-// console.log("休憩しおう！")
+import 'jquery-ui/ui/widgets/datepicker.js';
+
+
+// $(document).ready(function() is depreciated ---> $(function()
+$(function() {
+    $('#submitJisho_histories').on('click', function(){
+        GetJishoHistories();
+    });
+});
+// document.addEventListener("DOMContentLoaded", function(){
+//     $('#submitJisho_histories').on('click', function(){
+//         GetJishoHistories();
+//     });
+// });
+
+
+function GetJishoHistories() {
+    console.log("Jquery OK"); 
+    // $.ajax({
+
+    //     url: "{{ route('jisho_histories') }}",
+    //     success: function (result) {
+
+    //         if (result) {
+    //             console.log(result); 
+
+    //     } 
+    // });
+}
--- resources/views/endpoints/list.blade.php
+++ resources/views/endpoints/list.blade.php
@@ -7,14 +7,16 @@
 <div class="container-fluid">
     <h2>Liste des endpoints</h2>
     <div class="endpointSearch">
-        <form class="" action="{{ route('jisho_histories') }}" method="GET">
+        {{-- <form class="" action="{{ route('jisho_histories') }}" method="GET"> --}}
+        <form class="" action="" method="GET">
             <h3 class="endpointTitle">/api/jisho_histories</h3>
             <div class="input-group">
                 <span class="badge bg-primary">GET</span>
                 <span class="input-group-text" id="basic-addon1">/api/jisho_histories</span>
                 <input type="hidden" class="form-control" placeholder="all" aria-label="all" aria-describedby="basic-addon1" disabled>
                 
-                <button type="submit" class="btn btn-outline-success">Submit</button>
+                <button id="submitJisho_histories" type="button" class="btn btn-outline-success" >Submit</button>
+                {{-- <button type="submit" class="btn btn-outline-success" >Submit</button> --}}
             </div>
         </form>
         <div class="resultEndpointCall"></div>
