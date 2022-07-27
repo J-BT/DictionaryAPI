f212570c040d08d9654fb31c4e2f846f1754a1b0
J-BT <jeanbaptiste.theroulde@gmail.com>
2022-07-27 18:20:58 +0200

    reinstallation laravel-mix ubuntu

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,3 @@
-mix.js('resources/js/app.js', 'public/js')
-.postCss('resources/css/app.css', 'public/css')
-.sass('resources/sass/app.scss', 'public/css')
-.sourceMaps();
+let mix = require('laravel-mix');
 
-if(mix.inProduction()){
-mix.version();
-} 
+mix.js('resources/js/app.js', 'js').sass('resources/sass/app.scss', 'css');
