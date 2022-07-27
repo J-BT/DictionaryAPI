c6fc6d4df1ba4426d89be1bb9179a134a7bdf61d
J-BT <jeanbaptiste.theroulde@gmail.com>
2022-07-27 19:23:24 +0200

    pb front end fixed

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,9 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .postCss('resources/css/app.css', 'public/css')
+    .version();
+
+// if(mix.inProduction()){
+//     mix.version();
+// } 
